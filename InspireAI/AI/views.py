62f210d3ceed7cf6282e083b3952fd3c32b32de3
Long Nguyen <zedsc03@gmail.com>
2024-05-15 07:55:43 +0700

    Update Project

--- InspireAI/AI/views.py
+++ InspireAI/AI/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def chatbot(request):
+    return render(request, 'chatbot.html')
--- InspireAI/InspireAI/settings.py
+++ InspireAI/InspireAI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AI'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'InspireAI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'AI/templates/ai'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
